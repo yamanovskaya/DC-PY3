93d89f635318bd8e9f74e0511e7cfff787fe1797
yamanovskaya <yamanovskaya.ayu@edu.spbstu.ru>
2023-02-26 21:38:48 +0300

    lab1 is done

--- Лабораторная 1/Очередь/task.py
+++ Лабораторная 1/Очередь/task.py
@@ -2,23 +2,34 @@
 My little Queue
 """
 from typing import Any
+import doctest
 
 
 class Queue:
     def __init__(self):
         """
         Очередь с помощью python list
-        TODO Описать где начало и конец очереди
+        Начало очереди это элемент списка с индексом 0 (появившийся в очереди раньше всего)
+        Конец очереди это элемент списка с индексом -1 (появившийся в очереди позже всего)
+            >>> queue_ = Queue()
+            >>> print(queue_.list_ == [] and isinstance(queue_.list_, list))
+            True
         """
-        ...  # TODO инициализировать список
+        self.list_: list = []
 
     def enqueue(self, elem: Any) -> None:
         """
         Добавление элемент в конец очереди
 
         :param elem: Элемент, который должен быть добавлен
+            >>> queue_ = Queue()
+            >>> queue_.enqueue('element 1')
+            >>> queue_.enqueue('element 2')
+            >>> queue_.enqueue('element 3')
+            >>> print(queue_.list_ == ['element 1', 'element 2', 'element 3'])
+            True
         """
-        ...  # TODO реализовать метод enqueue
+        self.list_.append(elem)
 
     def dequeue(self) -> Any:
         """
@@ -27,8 +38,23 @@ class Queue:
         :raise: IndexError - Ошибка, если очередь пуста
 
         :return: Извлеченный с начала очереди элемент.
+
+            >>> queue_ = Queue()
+            >>> queue_.enqueue('element 1')
+            >>> queue_.enqueue('element 2')
+            >>> queue_.enqueue('element 3')
+            >>> print(queue_.dequeue() == 'element 1', queue_.list_)
+            True ['element 2', 'element 3']
+            >>> queue_ = Queue()
+            >>> a = queue_.dequeue()
+            Queue is empty
         """
-        ...  # TODO реализовать метод dequeue
+        try:
+            if len(self.list_) == 0:
+                raise IndexError('Queue is empty')
+            return self.list_.pop(0)
+        except IndexError:
+            print('Queue is empty')
 
     def peek(self, ind: int = 0) -> Any:
         """
@@ -40,13 +66,43 @@ class Queue:
         :raise: IndexError - если индекс вне границ очереди
 
         :return: Значение просмотренного элемента
+
+            >>> queue_ = Queue()
+            >>> queue_.enqueue('element 1')
+            >>> queue_.enqueue('element 2')
+            >>> queue_.enqueue('element 3')
+            >>> print(queue_.peek(), queue_.list_)
+            element 1 ['element 1', 'element 2', 'element 3']
         """
-        ...  # TODO реализовать метод peek
+        if not isinstance(ind, int):
+            raise TypeError('index must be int')
+        if not abs(ind) < len(self) - 1:
+            raise IndexError('index is out of range')
+        return self.list_[ind]
 
     def clear(self) -> None:
-        """ Очистка очереди. """
-        ...  # TODO реализовать метод clear
+        """ Очистка очереди.
+            >>> queue_ = Queue()
+            >>> queue_.enqueue('element 1')
+            >>> queue_.enqueue('element 2')
+            >>> queue_.enqueue('element 3')
+            >>> queue_.clear()
+            >>> print(queue_.list_)
+            []
+        """
+        # del self.list_
+        self.list_.clear()
 
     def __len__(self):
-        """ Количество элементов в очереди. """
-        ...  # TODO реализовать метод __len__
+        """ Количество элементов в очереди.
+            >>> queue_ = Queue()
+            >>> queue_.enqueue('element 1')
+            >>> queue_.enqueue('element 2')
+            >>> queue_.enqueue('element 3')
+            >>> print(len(queue_.list_))
+            3
+            """
+        return len(self.list_)
+
+
+doctest.testmod()  # тестирование примеров
