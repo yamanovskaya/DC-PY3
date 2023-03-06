0a0a794557a147a8866c7adc7cf9b9181af0793b
yamanovskaya <yamanovskaya.ayu@edu.spbstu.ru>
2023-03-06 10:30:25 +0300

    lab 2 done

--- Лабораторная 2/Итеративный линейный поиск/task.py
+++ Лабораторная 2/Итеративный линейный поиск/task.py
@@ -11,4 +11,16 @@ def min_search(arr: List[int]) -> int:
     :param arr: Массив целых чисел
     :return: Индекс первого вхождения элемента в массиве
     """
-    ...  # TODO реализовать итеративный линейный поиск
+    arr_len = len(arr)
+    if arr_len == 0:
+        raise ValueError
+    elif arr_len == 1:
+        return 0
+    else:
+        elem_temp = arr[0]
+        ind_temp = 0
+        for i in range(arr_len - 1):
+            if arr[i + 1] < elem_temp:
+                elem_temp = arr[i + 1]
+                ind_temp = i + 1
+        return ind_temp
