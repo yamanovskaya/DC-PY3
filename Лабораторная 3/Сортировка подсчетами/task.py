37f4821be2fb35f27230cec0febf9ee24c006c8a
yamanovskaya <115175074+yamanovskaya@users.noreply.github.com>
2023-03-22 20:08:21 +0300

    lab 3 task 1 updated

--- Лабораторная 3/Сортировка подсчетами/task.py
+++ Лабораторная 3/Сортировка подсчетами/task.py
@@ -13,3 +13,17 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :return: Отсортированный в порядке возрастания массив
     """
     ...  # TODO реализовать алгоритм сортировки подсчетами
+    temp: dict = {}
+    ans: list = []
+    for elem in container:
+        if elem in temp:
+            temp[elem] += 1
+        else:
+            temp[elem] = 1
+    for key, value in sorted(temp.items(), key= lambda x: x[0]):
+        ans.extend([key]*value)
+    return ans
+        
+       
+        
+        
